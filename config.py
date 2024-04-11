c6ccb5b0f3a271f88112b1c0dbecf7c734ea8808
ZAXph <152623389+ZAXph@users.noreply.github.com>
2024-04-11 19:54:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-IAM_TOKEN = "t1.9euelZrOyprOncaUm5vGkY6MnZiWx-3rnpWamMyalJebnZOKxsyVypOajJTl8_d6RCtP-e8fDxoo_N3z9zpzKE_57x8PGij8zef1656VmomTm5GQycuLmJ6cj5DLi82T7_zF656VmomTm5GQycuLmJ6cj5DLi82TveuelZqQno-OjpyNkZXJlYnKkI-Zk7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.GcgOBOlsejiPy4OkBjdFtNhepod0LQXRYLc9NtGy3EPJIQcRZOvnO3GDrEZhrarrkekjzmSOM9HS1CJ68MZOAw"
-FOLDER_ID = "b1gn1kghpq4rvsp4e2h1"
+IAM_TOKEN = ""
+FOLDER_ID = ""
 
 MAX_USER_TTS_SYMBOLS = 2000
 MAX_TTS_SYMBOLS = 500
 
-TOKEN = "6815594086:AAFmwexlJBjfNt8xinJKVhUz2613ND2opX0"
+TOKEN = ""
 URL = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
 VOICE = 'filipp'
 
